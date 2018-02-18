4d48c90f82a93a44dea1fc9fad402bce1acc3356
Pavan Dhareshwar <pavan.dhareshwar@colorado.edu>
2018-02-17 22:16:39 -0700

    1. Added code in the thread 1 routine to create a linked list of character read from a input file and print all the characters that occur #REP_COUNT number of times. 2. Added code in thread 2 routine to log CPU utilization every 100 ms. 3. These threads keep executing until the user signals SIGUSR1 or SIGUSR2 is sent to terminate these threads.

--- HomeworkDir/HW3/multiThreadProg.c
+++ HomeworkDir/HW3/multiThreadProg.c
@@ -21,13 +21,14 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+	/* 	Variable to hold the log file name specified by the user */
     char log_file_name[FNAME_BUF_LEN];
     memset(log_file_name, '\0', sizeof(log_file_name));
 	strncpy(log_file_name, argv[1], strlen(argv[1]));
 
     printf("Log File Name: %s\n", log_file_name);
 
-    /* Check if the file exists, if not create a file with R+W permissions */
+    /* 	Check if the file exists, if not create a file with R+W permissions */
     if (!fopen(log_file_name, "r"))
     {
         printf("Log file %s doesn't exist. Creating one.\n", log_file_name);
@@ -44,11 +45,15 @@ int main(int argc, char *argv[])
         }
     }
 
+	/* 	Create and initialize the thread info structure that will be passed on 
+		to the master and two child threads
+	*/
     x_thread_info thread_info;
     memset(&thread_info, '\0', sizeof(x_thread_info));
 
     strcpy(thread_info.log_file_name, log_file_name);
 
+	/* Initialize the binary semaphore */
     sem_init(&g_sem_lfile_write, 0, 1);
 
     int thread_cret_ret_val = create_threads(thread_info);
@@ -123,15 +128,13 @@ void *mt_func(void *arg)
 	sprintf(buffer, "[Master] Thread start at %s (POSIX tid: %u, Linux tid: %d)\n", time_str, (unsigned int)posix_tid, linux_tid);
 	write_to_log_file(buffer, strlen(buffer), fp_master);
 	
-	//signal(SIGUSR1, sig_handler_ct1);
-	signal(SIGUSR2, sig_handler_ct1);
-	signal(SIGUSR1, sig_handler_ct2);
-	//signal(SIGUSR2, sig_handler_ct2);
+	signal(SIGUSR1, sig_handler_ct1);
+	//signal(SIGUSR2, sig_handler_ct1);
+	//signal(SIGUSR1, sig_handler_ct2);
+	signal(SIGUSR2, sig_handler_ct2);
 
-	sleep(2);
-
-	pthread_kill(t_child_1_id, SIGUSR1);
-	pthread_kill(t_child_2_id, SIGUSR2);
+	//pthread_kill(t_child_1_id, SIGUSR1);
+	//pthread_kill(t_child_2_id, SIGUSR2);
 
     pthread_join(t_child_1_id, NULL);
     pthread_join(t_child_2_id, NULL);
@@ -154,13 +157,18 @@ void sig_handler_ct1(int sig)
 	{
 		printf("Caught signal in child thread 1: %d\n", sig);
 
-		sprintf(buffer, "[Child 1] Thread exit\n");
+		/* 	Time when the thread started */
+		time_t start_time = time(NULL); 
+		char *time_str = ctime(&start_time);
+
+		sprintf(buffer, "[Child 1] Thread exit at %s\n", time_str);
 		write_to_log_file(buffer, strlen(buffer), fp_ct1);
 
 		if (fp_ct1)
 			fclose(fp_ct1);
 	
-		pthread_exit(NULL);
+		//pthread_exit(NULL);
+		pthread_cancel(t_child_1_id);
 	}
 }
 
@@ -173,14 +181,120 @@ void sig_handler_ct2(int sig)
 	{
 		printf("Caught signal in child thread 2: %d\n", sig);
 		
-		sprintf(buffer, "[Child 2] Thread exit\n");
+		/* 	Time when the thread started */
+		time_t start_time = time(NULL); 
+		char *time_str = ctime(&start_time);
+
+		sprintf(buffer, "[Child 2] Thread exit at %s\n", time_str);
 		write_to_log_file(buffer, strlen(buffer), fp_ct2);
 
 		if (fp_ct2)	
 			fclose(fp_ct2);
 	
-		pthread_exit(NULL);
+		//pthread_exit(NULL);
+		pthread_cancel(t_child_2_id);
+
+		return;
 	}
+}
+
+char to_lower(char ch)
+{
+	if (ch >= 'A' && ch <= 'Z')
+		return (ch - 'A' + 'a');
+	return ch; 
+}
+
+void add_node_to_list(struct node_t **head, int c)
+{
+	struct node_t *current = (*head), *prev = NULL;
+
+	if (head != NULL && current == NULL)
+	{
+		/* Linked list is empty */
+		struct node_t *new_node = (struct node_t *)malloc(sizeof(struct node_t));
+	
+		new_node->ch = c;
+		new_node->count = 1;
+		new_node->next = NULL;
+
+		(*head) = new_node;	
+		return;	
+	}
+
+	while(current != NULL)
+	{
+		if ((char)current->ch == (char)c || (char)current->ch == to_lower((char)c))
+		{	
+			current->count++;
+			break;
+		}
+
+		prev = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		/* 	The node corresponding to the character doesn't exist, we will be 
+			creating one and setting the count as 1.	
+		*/
+		struct node_t *new_node = (struct node_t *)malloc(sizeof(struct node_t));
+	
+		new_node->ch = c;
+		new_node->count = 1;
+		new_node->next = NULL;
+
+		prev->next = new_node;
+	}
+
+	return;
+}
+
+void printList(struct node_t *head)
+{
+	struct node_t *current = head;
+
+	while (current != NULL)
+	{
+		printf("Character is %c with count %d\n", current->ch, current->count);
+		current = current->next;
+	}
+}
+
+void create_list(struct node_t **head)
+{
+	FILE *fp_txt_file = fopen(INP_TXT_FILE, "r");
+	if (!fp_txt_file)
+	{
+		printf("File %s open failed\n", INP_TXT_FILE);
+	}
+
+	int c;
+
+	do
+	{
+		c = fgetc(fp_txt_file);
+		add_node_to_list(head, c);
+		if (feof(fp_txt_file))
+			break;
+	}while(1);
+}
+
+void find_nodes_with_mult_occurrence(struct node_t *head)
+{
+	struct node_t *current = head;
+
+	printf("Character with %d count: ", REP_COUNT);
+	while(current != NULL)
+	{
+		if (current->count == REP_COUNT)
+		{
+			printf("%c ", (char)current->ch);
+		}
+		current = current->next;
+	}
+	printf("\n");
 }
 
 void *ct1_func(void *arg)
@@ -200,18 +314,31 @@ void *ct1_func(void *arg)
 	pthread_t posix_tid = pthread_self();
 	pid_t linux_tid = syscall( __NR_gettid );
 
-	/* Time when the thread started */
+	/* 	Time when the thread started */
 	time_t start_time = time(NULL); 
 	char *time_str = ctime(&start_time);
 
 	sprintf(buffer, "[Child 1] Thread start at %s (POSIX tid: %u, Linux tid: %d)\n", time_str, (unsigned int)posix_tid, linux_tid);
 	write_to_log_file(buffer, strlen(buffer), fp_ct1);
 
+	/* 	Go through the text file and create a linked list with a node for each 
+	    character and store the character count in the count variable. Once the
+		entire file is read and the linked list is updated, we traverse the list
+		and find the character that has a count of 3(case is ignored).   
+	*/
 	while(1)
 	{
-		printf("In child 1\n");
-		sleep(1);
+		struct node_t *head = NULL;
+		struct node_t **last_ref_ptr = &head;
+		create_list(last_ref_ptr);
+
+		//printList(*last_ref_ptr);
+
+		find_nodes_with_mult_occurrence(*last_ref_ptr);
+	
+		sleep(2);
 	}
+
 }
 
 void *ct2_func(void *arg)
@@ -227,21 +354,67 @@ void *ct2_func(void *arg)
 		printf("Log file open failed in child 2\n");
 	}
 
-	/* POSIX and linux thread id information */
+	/* 	POSIX and linux thread id information */
 	pthread_t posix_tid = pthread_self();
 	pid_t linux_tid = syscall( __NR_gettid );
 
-	/* Time when the thread started */
+	/* 	Time when the thread started */
 	time_t start_time = time(NULL); 
 	char *time_str = ctime(&start_time);
 
 	sprintf(buffer, "[Child 2] Thread start at %s (POSIX tid: %u, Linux tid: %d)\n", time_str, (unsigned int)posix_tid, linux_tid);
 	write_to_log_file(buffer, strlen(buffer), fp_ct2);
 
+	/*	Varaible to hold the output of /proc/stat file read operation
+		
+		Reference: www.linuxhowtos.org/System/procstat.htm
+	
+		cpu user nice system idle iowait irq softirq
+		
+		The numbers captured identify the amount of time CPU has spent perfoming 
+		different kind of work.
+	
+		cpu 	--> aggregrates the number in all the other "cpuN" lines
+		
+		user 	--> normal processes executing in user mode
+		nice 	--> niced processes executing in user mode
+		system	--> processes executing in kernel mode
+		idle 	--> twiddling thumbs
+		iowait 	--> waiting for I/O to complete
+		irq		--> servicing interrupts
+		softirq	--> servicing soft interrupts	
+	 
+	*/
+
 	while(1)
 	{
-		printf("In child 2\n");
-		sleep(1);
+		//long long int a[7];
+		//char cpu[4];
+		//memset(cpu, '\0', sizeof(cpu));
+
+		FILE *fp = fopen("/proc/stat", "r");
+		//fscanf(fp, "%s %Ld %Ld %Ld %Ld %Ld %Ld %Ld", cpu, &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6]);
+		//printf("CPU ultilization:\n %s %Ld %Ld %Ld %Ld %Ld %Ld %Ld\n\n", cpu, a[0], a[1], a[2], a[3], a[4], a[5], a[6]);
+		char stat_buffer[3*MSG_BUF_LEN];
+		memset(stat_buffer, '\0', sizeof(stat_buffer));
+
+		char cpu_util_str[] = "\n\nCPU Utilization: \n";
+		strcpy(stat_buffer, cpu_util_str);
+		int count = 0;
+
+		count += strlen(cpu_util_str);
+
+		while (!feof(fp))
+		{
+			stat_buffer[count++] = fgetc(fp);
+		}
+		stat_buffer[count++] = '\n';
+	
+		write_to_log_file(stat_buffer, count-1, fp_ct2);
+	
+		fclose(fp);
+		
+		usleep(100000);
 	}
 }
 
--- HomeworkDir/HW3/multiThreadProg.h
+++ HomeworkDir/HW3/multiThreadProg.h
@@ -1,41 +1,58 @@
 #ifndef _MULTI_THREAD_H_
 #define _MULTI_THREAD_H_
 
-/*--------------------------- MACROS ------------------------*/
+/*----------------------------------- MACROS --------------------------------*/
+
 #define MSG_BUF_LEN			(1024)
 #define FNAME_BUF_LEN		(256)
 
-/*--------------------------- MACROS ------------------------*/
+#define INP_TXT_FILE		"Valentinesday.txt"
+#define REP_COUNT			3
 
-/*--------------------------- GLOBALS -----------------------*/
+/*----------------------------------- MACROS --------------------------------*/
 
+/*---------------------------------- GLOBALS --------------------------------*/
+
+/* 	Thread handles for 3 threads */
 pthread_t t_master_id, t_child_1_id, t_child_2_id;
 
+/* 	Semaphore used for mutually exclusive access of log file */
 sem_t g_sem_lfile_write;
 
+/* 	File handles for the log file each owned by 3 threads */
 FILE *fp_master = NULL;
 FILE *fp_ct1 = NULL;
 FILE *fp_ct2 = NULL;
 
-/*--------------------------- GLOBALS -----------------------*/
+/*---------------------------------- GLOBALS --------------------------------*/
 
-/*------------------ STRUCTURES/ENUMERATIONS-----------------*/
+/*---------------------------- STRUCTURES/ENUMERATIONS ----------------------*/
 
+/*	Thread info structure to pass arguments to the threads */
 typedef struct _x_thread_info
 {
     char log_file_name[FNAME_BUF_LEN];
 }x_thread_info; 
 
-/*------------------ STRUCTURES/ENUMERATIONS-----------------*/
+/* 	Linked list node used to store character, its count and pointer to next 
+	node
+*/
+struct node_t 
+{
+	int ch;
+	int count;
+	struct node_t *next;
+};
 
-/*------------------ FUNCTION PROTOTYPES --------------------*/
+/*---------------------------- STRUCTURES/ENUMERATIONS ----------------------*/
+
+/*---------------------------- FUNCTION PROTOTYPES --------------------------*/
 /**
  *  @brief Creates all the threads
  *  
- *  This function creates the master and the two child threads
- *  that will be used in this program. Each thread created will
- *  be passed as its argument a thread_info structure containing 
- *  the log file.
+ *  This function creates the master and the two child threads that will be 
+ *	used in this program. Each thread created will be passed as its argument 
+ * 	a thread_info structure containing the log file.
 
  *  @param thread_info : thread_info structure 
  *
@@ -45,18 +62,151 @@ typedef struct _x_thread_info
 
 int create_threads(x_thread_info thread_info);
 
+/**
+ *  @brief Start routine for master thread
+ *  
+ *  This function acts as the start routine for the master thread and is invoked 
+ *	when the master thread is created. The function is executed and return from 
+ *	from this function or invocation of pthread_exit marks the termination of 
+ *	the thread. 
+
+ *  @param arg : argument passed to mt_func
+ *
+ *  @return void
+*/
 void *mt_func(void *arg);
 
+/**
+ *  @brief Start routine for child thread 1
+ *  
+ *  This function acts as the start routine for child thread 1 and is invoked 
+ *	when child thread 1 is created. The function is executed and return from 
+ *	from this function or invocation of pthread_exit marks the termination of 
+ *	the thread. 
+
+ *  @param arg : argument passed to ct1_func
+ *
+ *  @return void
+*/
 void *ct1_func(void *arg);
 
+/**
+ *  @brief Start routine for child thread 2
+ *  
+ *  This function acts as the start routine for child thread 2 and is invoked 
+ *	when child thread 2 is created. The function is executed and return from 
+ *	from this function or invocation of pthread_exit marks the termination of 
+ *	the thread. 
+
+ *  @param arg : argument passed to ct2_func
+ *
+ *  @return void
+*/
 void *ct2_func(void *arg);
 
+/**
+ *  @brief Log file write function
+ *  
+ *  This function provides synchronized write access to the log file by the 
+ *	master and 2 child threads. 
+
+ *  @param buffer: 	pointer to buffer holding the message to be written to 
+ *					log file
+ *	@param len: 	message length
+ *	@param fp:		log file pointer
+ *
+ *  @return void
+*/
 void write_to_log_file(char *buffer, int len, FILE *fp);
 
+/**
+ *  @brief Signal handler for child thread 1
+ *  
+ *  The signal handler defined for handling SIGUSR1 and SIGUSR2 signal
+ *	delivered to child thread 1.
+
+ *	@param sig: 	signal number
+ *
+ *  @return void
+*/
 void sig_handler_ct1(int sig);
 
+/**
+ *  @brief Signal handler for child thread 2
+ *  
+ *  The signal handler defined for handling SIGUSR1 and SIGUSR2 signal
+ *	delivered to child thread 2.
+
+ *	@param sig: 	signal number
+ *
+ *  @return void
+*/
 void sig_handler_ct2(int sig);
 
-/*------------------ FUNCTION PROTOTYPES --------------------*/
+/**
+ *  @brief Helper function to convert to lower case character
+ *  
+ *  This function helps with the conversion of upper case character to
+ *	lower case.
+
+ *	@param ch: 		upper/lower case input character
+ *
+ *  @return char:	converted lower case character or original lower
+					case character
+*/
+char to_lower(char ch);
+
+/**
+ *  @brief Function to add a node to the linked list
+ *  
+ *  This function adds a node that will hold the character specified 
+ * 	by @param c if such a node doesn't exist, or increments the count
+ *	held by the previously created node holding the character.
+
+ *	@param head:	Pointer to head of the linked list
+ *	@param c:		Character to be added to linked list
+ *
+ *  @return void
+*/
+void add_node_to_list(struct node_t **head, int c);
+
+/**
+ *  @brief Function to print the nodes of the linked list
+ *  
+ *  This function will print the characters and the count of the character
+ *	held by the nodes of the linked list. 
+
+ *	@param head:	Head of the linked list
+ *
+ *  @return void
+*/
+void printList(struct node_t *head);
+
+/**
+ *  @brief Function to create a linked list of characters and its count
+ *  
+ *  This function creates a linked list with nodes holding information on
+ * 	characters and its count present in a file specified by macro INP_TXT_FILE
+
+ *	@param head:	Pointer to head of the linked list
+ *
+ *  @return void
+*/
+void create_list(struct node_t **head);
+
+/**
+ *  @brief Function to find nodes with specified number of occurrences
+ *  
+ *  This function is used to traverse through the linked list specified by 
+ *	@param head and find nodes that contains #REP_COUNT number of character
+ * 	occurrences recorded by count member of the linked list node.
+
+ *	@param head:	Head of the linked list
+ *
+ *  @return void
+*/
+void find_nodes_with_mult_occurrence(struct node_t *head);
+
+/*---------------------------- FUNCTION PROTOTYPES --------------------------*/
 
 #endif // #ifndef _MULTI_THREAD_H_ 
